d3894b96674c53fec0e8f04605a1f8aff1a0deaa
sashavoloshanov <svoloshanov@icloud.com>
2016-01-02 10:27:32 +0200

    111

---
+++ FriendsssssApp/FriendsssssApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendsssssApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
